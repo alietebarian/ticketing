ed07078071dff71ef0f2df793d10a634effd633b
Ali Etebarain <alietebarianbusiness@gmail.com>
2024-07-14 11:07:00 +0330

    test

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import MainPage from "./MainPage";
 import SIdeBar from "./component/SIdeBar";
 import { QueryClient, QueryClientProvider } from "react-query";
 
+const one = 1;
+
 export default function App() {
 
   const queryClient = new QueryClient();
-
-
   return (
     <div className="p-1.5 flex">
       <QueryClientProvider client={queryClient}>
